a58a91eb9080059169cb97b528cdce71b3deb27a
sd42 <info@sd42.nl>
2023-11-02 15:53:16 +0100

    Teacher-provided template

---
+++ file_reader_module.py
@@ -0,0 +1,2 @@
+# This module implements functionality to read the procedure history stored in a a CSV file.
+# After the file has been processed the data is returned in a proper data structure.
---
+++ main.py
@@ -0,0 +1,1 @@
+# Add your code here. Don't forget to write efficient and easy to read code. Good luck!
